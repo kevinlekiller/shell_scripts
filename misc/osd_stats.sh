ae1fb3c6c188f300ca97b63514f711e49a0b0278
kevinlekiller <kevinlekiller@users.noreply.github.com>
2021-06-04 17:45:28 -0400

    Update osd_stats.sh

--- misc/osd_stats.sh
+++ misc/osd_stats.sh
@@ -28,7 +28,8 @@ LICENSE
 # In the Trigger, add the keyboard shortcut.
 # In the Action, paste the command above.
 
-for device in amdgpu it8665; do
+i=0
+for device in amdgpu it8665 zenpower; do
     tmpPath=$(grep "$device" /sys/class/hwmon/hwmon*/name | grep -Po "^/sys/class/hwmon/hwmon\d+")
     if [[ -z $tmpPath ]]; then
         echo "Could not find hwmon path for device '$device'."
@@ -37,7 +38,7 @@ for device in amdgpu it8665; do
     eval "${device}_dir"="$tmpPath"
 done
 
-osd_delay=2
+osd_delay=3
 osd_position=top
 osd_align=right
 osd_top_offset=5
@@ -46,63 +47,90 @@ osd_outline=2
 osd_color=green
 osd_font="-*-*-*-*-*-*-20-*-*-*-*-*-*-*"
 
-valName[0]=CPU
-valType[0]=MHz
-valLoc_[0]="/proc/cpuinfo"
+valName[$i]=CPU
+valType[$i]=MHz
+valLoc_[$i]="/proc/cpuinfo"
+((++i))
 
-valName[1]=CPU
-valType[1]=C
-valLoc_[1]="$it8665_dir/temp1_input"
+valName[$i]=CPU
+valType[$i]=W
+valLoc_[$i]="$zenpower_dir/power1_input"
+((++i))
 
-valName[2]=CPU
-valType[2]=RPM
-valLoc_[2]="$it8665_dir/fan1_input"
+valName[$i]=CPU
+valType[$i]=mV
+valLoc_[$i]="$zenpower_dir/in1_input"
+((++i))
 
-valName[3]=Chassis
-valType[3]=RPM
-valLoc_[3]="$it8665_dir/fan5_input"
+valName[$i]="CPU SOC"
+valType[$i]=W
+valLoc_[$i]="$zenpower_dir/power2_input"
+((++i))
 
-valName[4]=GPU
-valType[4]=RPM
-valLoc_[4]="$amdgpu_dir/fan1_input"
+valName[$i]=CPU
+valType[$i]=C
+valLoc_[$i]="$it8665_dir/temp1_input"
+((++i))
 
-valName[5]=GPU
-valType[5]=mV
-valLoc_[5]="$amdgpu_dir/in0_input"
+valName[$i]=CPU
+valType[$i]=RPM
+valLoc_[$i]="$it8665_dir/fan1_input"
+((++i))
 
-valName[6]="GPU Load"
-valType[6]=%
-valLoc_[6]="$amdgpu_dir/device/gpu_busy_percent"
+valName[$i]=Chassis
+valType[$i]=RPM
+valLoc_[$i]="$it8665_dir/fan5_input"
+((++i))
 
-valName[7]=GPU
-valType[7]=W
-valLoc_[7]="$amdgpu_dir/power1_average"
+valName[$i]=GPU
+valType[$i]=RPM
+valLoc_[$i]="$amdgpu_dir/fan1_input"
+((++i))
 
-valName[8]=GPU
-valType[8]=MHz
-valLoc_[8]="$amdgpu_dir/device/pp_dpm_sclk"
+valName[$i]=GPU
+valType[$i]=mV
+valLoc_[$i]="$amdgpu_dir/in0_input"
+((++i))
 
-valName[9]="GPU HBM"
-valType[9]=MHz
-valLoc_[9]="$amdgpu_dir/device/pp_dpm_mclk"
+valName[$i]="GPU Load"
+valType[$i]=%
+valLoc_[$i]="$amdgpu_dir/device/gpu_busy_percent"
+((++i))
 
-valName[10]=GPU
-valType[10]=C
-valLoc_[10]="$amdgpu_dir/temp1_input"
+valName[$i]=GPU
+valType[$i]=W
+valLoc_[$i]="$amdgpu_dir/power1_average"
+((++i))
 
-valName[11]="GPU HBM"
-valType[11]=C
-valLoc_[11]="$amdgpu_dir/temp3_input"
+valName[$i]=GPU
+valType[$i]=MHz
+valLoc_[$i]="$amdgpu_dir/device/pp_dpm_sclk"
+((++i))
 
-valName[12]="GPU jnc"
-valType[12]=C
-valLoc_[12]="$amdgpu_dir/temp2_input"
+valName[$i]="GPU HBM"
+valType[$i]=MHz
+valLoc_[$i]="$amdgpu_dir/device/pp_dpm_mclk"
+((++i))
 
-valName[13]=VRAM
-valType[13]=MB
-valLoc_[13]="$amdgpu_dir/device/mem_info_vram_used"
+valName[$i]=GPU
+valType[$i]=C
+valLoc_[$i]="$amdgpu_dir/temp1_input"
+((++i))
 
-vals=14
+valName[$i]="GPU HBM"
+valType[$i]=C
+valLoc_[$i]="$amdgpu_dir/temp3_input"
+((++i))
+
+valName[$i]="GPU jnc"
+valType[$i]=C
+valLoc_[$i]="$amdgpu_dir/temp2_input"
+((++i))
+
+valName[$i]=VRAM
+valType[$i]=MB
+valLoc_[$i]="$amdgpu_dir/device/mem_info_vram_used"
+((++i))
 
 ###########################################################
 ###########################################################
@@ -124,13 +152,13 @@ function catchExit() {
     pkill osd_cat
     exit 0
 }
-((--vals))
-osd_lines="$((vals+2))"
+((--i))
+osd_lines="$((i+2))"
 sleep_delay=$(bc -l <<< "$osd_delay-0.1")
-for i in $(seq 0 $vals); do
-    string="$string$(printf "%-8s%6s%-3s" "${valName[$i]}" "" "${valType[$i]}")\n"
+for j in $(seq 0 "$i"); do
+    string="$string$(printf "%-8s%6s%-3s" "${valName[$j]}" "" "${valType[$j]}")\n"
     if [[ ! -f ${valLoc_[$i]} ]]; then
-        echo "Error: Sensor path not found: '${valLoc_[$i]}'"
+        echo "Error: Sensor path not found: '${valLoc_[$j]}'"
         exit 1
     fi
 done
@@ -138,19 +166,19 @@ printOSD "-1"
 osd_side_offset="$((osd_side_offset+40))"
 while true; do
     string=""
-    for i in $(seq 0 $vals); do
-        if [[ ${valType[$i]} == C ]]; then
-            string="$string$(($(cat "${valLoc_[$i]}")/1000))\n"
-        elif [[ ${valType[$i]} == W ]]; then
-            string="$string$(($(cat "${valLoc_[$i]}")/1000000))\n"
-        elif [[ ${valType[$i]} == MB ]]; then
-            string="$string$(($(cat "${valLoc_[$i]}")/1048576))\n"
-        elif [[ ${valName[$i]} == CPU && ${valType[$i]} == MHz ]]; then
-            string="$string$(grep MHz "${valLoc_[$i]}"  | cut -d: -f2 | cut -d. -f1 | sort -nr | head -n1 | sed "s/ *//g")\n"
-        elif [[ ${valType[$i]} == MHz ]]; then
-            string="$string$(grep "\*" "${valLoc_[$i]}" | grep -Po "[0-9]{3,4}")\n"
+    for j in $(seq 0 "$i"); do
+        if [[ ${valType[$j]} == C ]]; then
+            string="$string$(($(cat "${valLoc_[$j]}")/1000))\n"
+        elif [[ ${valType[$j]} == W ]]; then
+            string="$string$(($(cat "${valLoc_[$j]}")/1000000))\n"
+        elif [[ ${valType[$j]} == MB ]]; then
+            string="$string$(($(cat "${valLoc_[$j]}")/1048576))\n"
+        elif [[ ${valName[$j]} == CPU && ${valType[$j]} == MHz ]]; then
+            string="$string$(grep MHz "${valLoc_[$j]}"  | cut -d: -f2 | cut -d. -f1 | sort -nr | head -n1 | sed "s/ *//g")\n"
+        elif [[ ${valType[$j]} == MHz ]]; then
+            string="$string$(grep "\*" "${valLoc_[$j]}" | grep -Po "[0-9]{3,4}")\n"
         else
-            string="$string$(cat "${valLoc_[$i]}")\n"
+            string="$string$(cat "${valLoc_[$j]}")\n"
         fi
     done
     printOSD "$osd_delay"
